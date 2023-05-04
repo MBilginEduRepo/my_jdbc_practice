4dd6e3bbb7a85de83c28a265430170e1fb9049ea
yavuz1990 <115427573+yavuz1990@users.noreply.github.com>
2023-05-04 14:58:32 +0300

    Updated Jdbc_Eaxamples.java

--- src/test/java/JDBC/Jdbc_Eaxamples.java
+++ src/test/java/JDBC/Jdbc_Eaxamples.java
@@ -1,4 +1,120 @@
 package JDBC;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+
 public class Jdbc_Eaxamples {
+
+        String dbUrl = "jdbc:oracle:thin:@3.91.68.84:1521:XE";
+        String dbUsername = "hr";
+        String dbPassword = "hr";
+
+        @Test
+        public void test1() throws SQLException {
+            Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("Select * from Departments");
+
+            //move to first row
+            //resultSet.next();
+
+            //System.out.println(resultSet.getString(2));
+            //display departments table in 10 - Administration - 200 - 1700 format
+
+            //System.out.println(resultSet.getInt(1) + " - " + resultSet.getString(2) + " - " + resultSet.getInt(3) + " - " + resultSet.getInt(4));
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1) + " - " + resultSet.getString(2) + " - " + resultSet.getInt(3) + " - " + resultSet.getInt(4));
+            }
+
+            //close connection
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+
+    }
+    @DisplayName("ResultSet Methods")
+    @Test
+    public void test2() throws SQLException {
+        Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+        ResultSet resultSet = statement.executeQuery("Select * from Departments ");
+
+        /*
+        resultSet.next();
+        resultSet.previous();
+        resultSet.beforeFirst();
+        resultSet.afterLast();
+         resultSet.absolute();
+        resultSet.getRow();*/
+
+
+        //how to find how many rows we have for the query
+        //System.out.println(resultSet.getInt(1));
+
+        //move to last row
+        resultSet.last();
+
+        //get the row count
+        int rowCount= resultSet.getRow();
+        System.out.println(rowCount);
+
+        //to move before first row after we use .last method
+        resultSet.beforeFirst();
+
+        //print all second column information
+        while(resultSet.next()){
+            System.out.println(resultSet.getString(2));
+        }
+
+        //close connection
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+
+
+    @DisplayName("ResultSet Methods")
+    @Test
+    public void test3() throws SQLException {
+        Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+        ResultSet resultSet = statement.executeQuery("Select * from Departments ");
+
+        //get the database related data inside the dbMetedata object
+        DatabaseMetaData dbMetadata=connection.getMetaData();
+
+        System.out.println("dbMetadata.getUserName() = " + dbMetadata.getUserName());
+        System.out.println("dbMetadata.getDatabaseProductName() = " + dbMetadata.getDatabaseProductName());
+        System.out.println("dbMetadata.getDatabaseProductVersion() = " + dbMetadata.getDatabaseProductVersion());
+        System.out.println("dbMetadata.getDriverName() = " + dbMetadata.getDriverName());
+        System.out.println("dbMetadata.getDriverVersion() = " + dbMetadata.getDriverVersion());
+
+
+
+        //close connection
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
