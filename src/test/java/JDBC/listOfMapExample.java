b18638df6c398cdb5bec457e223e965717db1d8c
yavuz1990 <115427573+yavuz1990@users.noreply.github.com>
2023-05-05 15:10:16 +0300

    Created dynamic_list.java

--- src/test/java/JDBC/listOfMapExample.java
+++ src/test/java/JDBC/listOfMapExample.java
@@ -48,59 +48,6 @@ public class listOfMapExample {
 
     }
 
-        @Test
-    public void test2() throws SQLException {
-            Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            ResultSet resultSet = statement.executeQuery("select FIRST_NAME,LAST_NAME,SALARY,JOB_ID\n" +
-                    "from EMPLOYEES\n" +
-                    "where ROWNUM < 6 ");
-
-            //in order ro get  column names we need resultsetmetadata
-            ResultSetMetaData rsmd= resultSet.getMetaData();
-            //move to first row
-            resultSet.next();
-
-            //creating list for keeping all the rows maps
-            List<Map<String,Object>> queryData= new ArrayList<>();
-
-            Map<String,Object> row1=new HashMap<>();
-
-            row1.put(rsmd.getColumnName(1),resultSet.getString(1));
-            row1.put(rsmd.getColumnName(2),resultSet.getString(2));
-            row1.put(rsmd.getColumnName(3),resultSet.getString(3));
-            row1.put(rsmd.getColumnName(4),resultSet.getString(4));
-
-            System.out.println(row1.toString());
-
-            //move to second row
-            resultSet.next();
-
-            Map<String,Object> row2 = new HashMap<>();
-
-            row2.put("first_name","Neena");
-            row2.put("last_name","Kochar");
-            row2.put("salary",17000);
-            row2.put("job_id","AD_VP");
-
-            System.out.println(row2.toString());
-
-            //adding rows one by one to my list
-            queryData.add(row1);
-            queryData.add(row2);
-
-            //get the steven lastname directly from the list
-            System.out.println(queryData.get(0).get("last_name"));
-
-            System.out.println(queryData.get(1).get("salary"));
-
-
-            //close connection
-            resultSet.close();
-            statement.close();
-            connection.close();
-
-        }
 
 
 
